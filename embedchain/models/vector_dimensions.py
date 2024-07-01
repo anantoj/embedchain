ed5cd396b071e0c7aad88bc8fc7013829192fd7f
Ananto Joyoadikusumo <ananto@Anantos-MacBook-Pro-2.local>
2024-07-01 12:55:32 +0700

    Update HF vector dimension

--- embedchain/models/vector_dimensions.py
+++ embedchain/models/vector_dimensions.py
@@ -6,7 +6,7 @@ class VectorDimensions(Enum):
     GPT4ALL = 384
     OPENAI = 1536
     VERTEX_AI = 768
-    HUGGING_FACE = 384
+    HUGGING_FACE = 1024
     GOOGLE_AI = 768
     MISTRAL_AI = 1024
     NVIDIA_AI = 1024
